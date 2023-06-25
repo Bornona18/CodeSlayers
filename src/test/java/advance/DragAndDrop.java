39c201c7f19a6f4fc004428e77554b623a01f8a0
bornona.nj@gmail.com <pagli@macbook-air.lan>
2023-06-25 16:13:25 -0400

    2nd commit

--- src/test/java/advance/DragAndDrop.java
+++ src/test/java/advance/DragAndDrop.java
@@ -1,40 +1,15 @@
 package advance;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DragAndDrop {
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		WebElement from = driver.findElement(By.id(""));
-
-		WebElement To = driver.findElement(By.id(""));
-		
-		Actions act = new Actions(driver);
-		Thread.sleep(1000);
-		act.dragAndDrop(from, To);
-		
-		
-		
-		
-		
-		
+	 
+	public static void main(String[] args) {
 		
 	}
+		
+		
+		
+		
+	
 
 }
