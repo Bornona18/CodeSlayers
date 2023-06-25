8aae34d2144d863549a0f3218a84fc45756e0a0f
bornona.nj@gmail.com <pagli@macbook-air.lan>
2023-06-25 16:13:03 -0400

    Coding for CodeSlayers project

---
+++ src/test/java/pages/GoogleHome.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleHome {
+	
+	WebDriver driver;
+	
+
+	public GoogleHome(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+		
+
+	
+	//WebElement searchBox = driver.findElement(By.name("q"));
+	
+	@FindBy(name = "q")
+	WebElement searchBox;
+	
+	//searchBox.sendKeys(string);
+	
+	public void enterSearch(String term) {
+		searchBox.sendKeys(term);
+	}
+	
+//	WebElement searchBtn =driver.findElement(By.name("btnK"));
+	@FindBy(name="btnK")
+	WebElement searchBtn;
+	
+	public void clickSearch() {
+		searchBtn.submit();
+	}
+	
+		
+
+}
